c3d4e59b93a81e7b83e9dbec6ed399ab88bc0942
Erwin Elder <erwineldermail@gmail.com>
2024-12-17 18:39:02 +0100

    Code refactoring.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/core/data/local/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/core/data/local/AppDatabase.kt
@@ -2,42 +2,15 @@ package cz.cvut.docta.core.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import cz.cvut.docta.answer.data.model.AnswerEntity
-import cz.cvut.docta.answer.data.model.AnswerPairEntity
-import cz.cvut.docta.core.data.model.AnswerPairsQuestionToPairEntity
-import cz.cvut.docta.core.data.model.LessonToQuestionEntity
 import cz.cvut.docta.core.data.model.LocaleEntity
-import cz.cvut.docta.core.data.model.QuestionToAnswerEntity
-import cz.cvut.docta.core.data.model.UserEntity
-import cz.cvut.docta.core.data.model.UserRoleEntity
 import cz.cvut.docta.course.data.model.CourseEntity
 import cz.cvut.docta.course.data.model.CourseSectionEntity
-import cz.cvut.docta.course.data.model.LessonEntity
-import cz.cvut.docta.question.data.model.AnswerPairsQuestionEntity
-import cz.cvut.docta.question.data.model.MultipleChoiceQuestionEntity
-import cz.cvut.docta.question.data.model.OpenAnswerQuestionEntity
-import cz.cvut.docta.question.data.model.QuestionEntity
-import cz.cvut.docta.question.data.model.SingleChoiceQuestionEntity
 
 @Database(
     entities = [
-        AnswerEntity::class,
-        AnswerPairEntity::class,
-        AnswerPairsQuestionToPairEntity::class,
         CourseSectionEntity::class,
-        LessonToQuestionEntity::class,
         LocaleEntity::class,
-        QuestionToAnswerEntity::class,
-        UserEntity::class,
-        UserRoleEntity::class,
-        CourseEntity::class,
-        CourseSectionEntity::class,
-        LessonEntity::class,
-        AnswerPairsQuestionEntity::class,
-        MultipleChoiceQuestionEntity::class,
-        OpenAnswerQuestionEntity::class,
-        QuestionEntity::class,
-        SingleChoiceQuestionEntity::class
+        CourseEntity::class
     ],
     version = 1,
     exportSchema = true
