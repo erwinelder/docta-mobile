ffc2ebd25c5f7dae05892fa3abe6211fa2e14f26
Erwin Elder <erwineldermail@gmail.com>
2024-12-17 18:54:45 +0100

    Add DAOs for Course, Course Section, and Course Locale.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/core/data/local/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/core/data/local/AppDatabase.kt
@@ -3,18 +3,23 @@ package cz.cvut.docta.core.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import cz.cvut.docta.core.data.model.LocaleEntity
+import cz.cvut.docta.course.data.local.CourseDao
+import cz.cvut.docta.course.data.local.CourseSectionDao
+import cz.cvut.docta.course.data.local.LocaleDao
 import cz.cvut.docta.course.data.model.CourseEntity
 import cz.cvut.docta.course.data.model.CourseSectionEntity
 
 @Database(
     entities = [
+        CourseEntity::class,
         CourseSectionEntity::class,
         LocaleEntity::class,
-        CourseEntity::class
     ],
     version = 1,
     exportSchema = true
 )
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun courseDao(): CourseDao
+    abstract fun courseSectionDao(): CourseSectionDao
+    abstract fun localeDao(): LocaleDao
 }
-
