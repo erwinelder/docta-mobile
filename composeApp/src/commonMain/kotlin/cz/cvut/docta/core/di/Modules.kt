44b5b3347b461f41b4f54e02c803190cf95944b5
Erwin Elder <erwineldermail@gmail.com>
2024-12-20 20:05:56 +0100

    Add GetAllCoursesUseCase to DI.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/core/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/core/di/Modules.kt
@@ -4,10 +4,18 @@ import cz.cvut.docta.course.data.local.source.CourseLocalDataSource
 import cz.cvut.docta.course.data.local.source.courseLocalDataSourceFactory
 import cz.cvut.docta.course.data.repository.CourseRepository
 import cz.cvut.docta.course.data.repository.CourseRepositoryImpl
+import cz.cvut.docta.course.domain.usecase.GetAllLightweightCoursesUseCase
+import cz.cvut.docta.course.domain.usecase.GetAllLightweightCoursesUseCaseImpl
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 expect val platformModule: Module
+
+val useCaseModule = module {
+    single<GetAllLightweightCoursesUseCase> {
+        GetAllLightweightCoursesUseCaseImpl(courseRepository = get())
+    }
+}
 
 val localDataSourceModule = module {
     single<CourseLocalDataSource> {
--- composeApp/src/commonMain/kotlin/cz/cvut/docta/core/di/initKoin.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/core/di/initKoin.kt
@@ -7,7 +7,7 @@ fun initKoin(config: KoinAppDeclaration? = null) {
     startKoin {
         config?.invoke(this)
         modules(
-            platformModule, localDataSourceModule, repositoryModule
+            platformModule, useCaseModule, localDataSourceModule, repositoryModule
         )
     }
 }
