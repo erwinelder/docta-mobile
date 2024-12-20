916be6ce369ea0fe9a14bb96c715f2453f002be6
Erwin Elder <erwineldermail@gmail.com>
2024-12-20 19:55:37 +0100

    Add Course mapper from data models to CourseLightweight domain model.

---
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/course/mapper/CourseMapper.kt
@@ -0,0 +1,14 @@
+package cz.cvut.docta.course.mapper
+
+import cz.cvut.docta.course.data.model.CourseEntity
+import cz.cvut.docta.course.domain.model.CourseLightweight
+
+fun List<CourseEntity>.toCourseLightweightList() = map { it.toCourseLightweight() }
+
+fun CourseEntity.toCourseLightweight(): CourseLightweight {
+    return CourseLightweight(
+        code = code,
+        locale = locale,
+        name = name
+    )
+}
