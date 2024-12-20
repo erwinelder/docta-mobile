b536ac195349bdec00facbc1efc0b1ee1d96b00b
Erwin Elder <erwineldermail@gmail.com>
2024-12-20 16:47:44 +0100

    Code refactoring.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/core/data/local/AppLocalDatabase.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/core/data/local/AppLocalDatabase.kt
@@ -5,9 +5,9 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import cz.cvut.docta.core.data.model.LocaleEntity
-import cz.cvut.docta.course.data.local.CourseDao
-import cz.cvut.docta.course.data.local.CourseSectionDao
-import cz.cvut.docta.course.data.local.LocaleDao
+import cz.cvut.docta.course.data.local.dao.CourseDao
+import cz.cvut.docta.course.data.local.dao.CourseSectionDao
+import cz.cvut.docta.course.data.local.dao.LocaleDao
 import cz.cvut.docta.course.data.model.CourseEntity
 import cz.cvut.docta.course.data.model.CourseSectionEntity
 import kotlinx.coroutines.Dispatchers
