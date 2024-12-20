5f17f213859bf4331baca8da9e2a7eabad365056
Erwin Elder <erwineldermail@gmail.com>
2024-12-20 19:32:36 +0100

    Add Koin modules. Add Course repository, Course local data source, and app local database to these modules.

---
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/core/di/Modules.kt
@@ -0,0 +1,22 @@
+package cz.cvut.docta.core.di
+
+import cz.cvut.docta.course.data.local.source.CourseLocalDataSource
+import cz.cvut.docta.course.data.local.source.courseLocalDataSourceFactory
+import cz.cvut.docta.course.data.repository.CourseRepository
+import cz.cvut.docta.course.data.repository.CourseRepositoryImpl
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+expect val platformModule: Module
+
+val localDataSourceModule = module {
+    single<CourseLocalDataSource> {
+        courseLocalDataSourceFactory(appLocalDatabase = get())
+    }
+}
+
+val repositoryModule = module {
+    single<CourseRepository> {
+        CourseRepositoryImpl(localSource = get())
+    }
+}
