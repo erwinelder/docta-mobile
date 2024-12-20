b830d61913d14abd6c935c144c776c9dd24ca20d
Erwin Elder <erwineldermail@gmail.com>
2024-12-20 19:39:11 +0100

    Remove AppLocalDatabase from the App component.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/core/presentation/App.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/core/presentation/App.kt
@@ -3,17 +3,13 @@ package cz.cvut.docta.core.presentation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import cz.cvut.docta.core.data.local.AppLocalDatabase
 import cz.cvut.docta.core.domain.app.AppTheme
 import cz.cvut.docta.core.presentation.other.AppBackground
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.koinInject
 
 @Composable
 @Preview
 fun App() {
-    val appLocalDb = koinInject<AppLocalDatabase>()
-
     val appTheme = AppTheme.Light
 
     MaterialTheme {
