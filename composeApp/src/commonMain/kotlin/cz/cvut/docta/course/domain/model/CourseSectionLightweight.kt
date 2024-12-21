8865c727f83a2c1dacc58511bf34c89e098b608e
Erwin Elder <erwineldermail@gmail.com>
2024-12-21 17:19:39 +0100

    Remove order number from CourseSectionLightweight since these domain models should be already ordered after mapping from data models.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/course/domain/model/CourseSectionLightweight.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/course/domain/model/CourseSectionLightweight.kt
@@ -2,7 +2,6 @@ package cz.cvut.docta.course.domain.model
 
 data class CourseSectionLightweight(
     val id: Long,
-    val orderNum: Int,
     val name: String,
     val statistics: CourseSectionStatistics
 )
