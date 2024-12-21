6b42d645f4a6030acf4b43398578a392c8f0f073
Erwin Elder <erwineldermail@gmail.com>
2024-12-21 16:49:31 +0100

    Add materials to Course domain model.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/course/domain/model/Course.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/course/domain/model/Course.kt
@@ -1,9 +1,11 @@
 package cz.cvut.docta.course.domain.model
 
+import cz.cvut.docta.materials.domain.model.MaterialsGroup
+
 data class Course(
     val code: String,
     val locale: String,
     val name: String,
-//    val materialsGroup: MaterialsGroup,
-    val sections: List<CourseSection>
+    val materialsGroup: MaterialsGroup,
+    val sections: List<CourseSectionLightweight>
 )
