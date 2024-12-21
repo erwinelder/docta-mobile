ba9c3a20dfa27ff4a4a612de3b16cd7603285b28
Erwin Elder <erwineldermail@gmail.com>
2024-12-21 17:30:13 +0100

    Update CourseSectionsScreenPreview.

---
+++ composeApp/src/androidMain/kotlin/cz/cvut/docta/course/presentation/screen/CourseSectionsScreenPreview.kt
@@ -0,0 +1,35 @@
+package cz.cvut.docta.course.presentation.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import cz.cvut.docta.core.presentation.screen.ScreenPreviewContainer
+import cz.cvut.docta.course.domain.model.CourseSectionLightweight
+import cz.cvut.docta.course.domain.model.CourseSectionStatistics
+
+@Preview(device = Devices.PIXEL_7_PRO)
+@Composable
+fun CourseSectionsScreenPreview() {
+    val courseSectionList = listOf(
+        CourseSectionLightweight(
+            id = 1,
+            name = "Section name 1",
+            statistics = CourseSectionStatistics(
+                correctAnswerAmount = 10, wrongAnswerAmount = 20
+            )
+        ),
+        CourseSectionLightweight(
+            id = 2,
+            name = "Section name 2",
+            statistics = CourseSectionStatistics(
+                correctAnswerAmount = 30, wrongAnswerAmount = 40
+            )
+        ),
+    )
+
+    ScreenPreviewContainer {
+        CourseSectionsScreen(
+            courseSectionList = courseSectionList
+        )
+    }
+}
