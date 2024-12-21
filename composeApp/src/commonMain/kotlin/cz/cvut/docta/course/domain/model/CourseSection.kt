3d00a8f2b58cbb6bd971d4fdb13126d5db1241f0
Erwin Elder <erwineldermail@gmail.com>
2024-12-21 16:50:48 +0100

    Change CourseSection to CourseSectionLightweight, remove lessons from section domain model.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/course/domain/model/CourseSection.kt
+++
@@ -1,9 +0,0 @@
-package cz.cvut.docta.course.domain.model
-
-data class CourseSection(
-    val id: Long,
-    val orderNum: Int,
-    val name: String,
-    val statistics: CourseSectionStatistics,
-    val lessons: List<LessonLightWeight>
-)
