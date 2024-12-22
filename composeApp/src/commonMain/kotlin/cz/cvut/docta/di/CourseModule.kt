3951c3edf4a4e29c7bdb3d20cb7260d958a1d760
Erwin Elder <erwineldermail@gmail.com>
2024-12-22 16:00:13 +0100

    Divide DI modules into separate files.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/di/CourseModule.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/di/CourseModule.kt
@@ -7,11 +7,8 @@ import cz.cvut.docta.course.data.repository.CourseRepositoryImpl
 import cz.cvut.docta.course.domain.usecase.GetAllCoursesUseCase
 import cz.cvut.docta.course.domain.usecase.GetAllCoursesUseCaseImpl
 import cz.cvut.docta.course.presentation.viewModel.CoursesViewModel
-import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
-
-expect val platformModule: Module
 
 val courseModule = module {
 
@@ -32,9 +29,5 @@ val courseModule = module {
             getAllCoursesUseCase = get()
         )
     }
-
-}
-
-val courseSectionModule = module {
 
 }
---
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/di/CourseSectionModule.kt
@@ -0,0 +1,7 @@
+package cz.cvut.docta.di
+
+import org.koin.dsl.module
+
+val courseSectionModule = module {
+    // TODO-COURSE-SECTIONS
+}
