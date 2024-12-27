b724cee1db1c978aaa834f16270c5e2e5c962573
Erwin Elder <erwineldermail@gmail.com>
2024-12-27 20:28:29 +0100

    Implement GetCourseSectionsUseCase.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/section/domain/usecase/GetCourseSectionsUseCaseImpl.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/section/domain/usecase/GetCourseSectionsUseCaseImpl.kt
@@ -2,12 +2,18 @@ package cz.cvut.docta.section.domain.usecase
 
 import cz.cvut.docta.section.data.repository.SectionRepository
 import cz.cvut.docta.section.domain.model.Section
+import cz.cvut.docta.section.domain.model.SectionStatistics
+import cz.cvut.docta.section.mapper.toSectionsLightweight
 
 class GetCourseSectionsUseCaseImpl(
     private val sectionRepository: SectionRepository
 ) : GetCourseSectionsUseCase {
     override suspend fun execute(courseCode: String): List<Section> {
-        // TODO-USECASE
-        return emptyList()
+        return sectionRepository.getCourseSections(courseCode)
+            .toSectionsLightweight()
+            .map {
+                // TODO-STATISTICS
+                Section(id = it.id, name = it.name, statistics = SectionStatistics())
+            }
     }
 }
