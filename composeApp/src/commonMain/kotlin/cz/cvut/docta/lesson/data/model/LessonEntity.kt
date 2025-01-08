062fb9df23a405082ff83366c0e9ecb7193b7663
Erwin Elder <erwineldermail@gmail.com>
2025-01-08 14:57:34 +0100

    Add foreign key to LessonEntity.

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/lesson/data/model/LessonEntity.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/lesson/data/model/LessonEntity.kt
@@ -1,9 +1,21 @@
 package cz.cvut.docta.lesson.data.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import cz.cvut.docta.section.data.model.SectionEntity
 
-@Entity(tableName = "lesson")
+@Entity(
+    tableName = "lesson",
+    foreignKeys = [
+        ForeignKey(
+            entity = SectionEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["sectionId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class LessonEntity(
     val sectionId: Long,
     @PrimaryKey(autoGenerate = true)
