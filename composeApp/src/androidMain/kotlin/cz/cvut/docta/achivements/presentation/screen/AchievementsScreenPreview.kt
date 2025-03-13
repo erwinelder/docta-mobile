52affa3b0af3fca803dc1e3c6af474fd09f72fe2
Maksym Zavada <m.zavada2005@ukr.net>
2025-03-13 22:36:44 +0100

    Update Achievements preview

--- composeApp/src/androidMain/kotlin/cz/cvut/docta/achivements/presentation/screen/AchievementsScreenPreview.kt
+++ composeApp/src/androidMain/kotlin/cz/cvut/docta/achivements/presentation/screen/AchievementsScreenPreview.kt
@@ -3,50 +3,51 @@ package cz.cvut.docta.achivements.presentation.screen
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import cz.cvut.docta.achievements.domain.model.Achievement
-import cz.cvut.docta.achievements.domain.model.AchievementName
-import cz.cvut.docta.achievements.presentation.AchievementsScreen
+import cz.cvut.docta.achievement.domain.model.Achievement
+import cz.cvut.docta.achievement.domain.model.AchievementName
+import cz.cvut.docta.achievement.presentation.AchievementsScreen
+import cz.cvut.docta.achievement.presentation.model.AchievementUiState
 import cz.cvut.docta.core.domain.app.AppTheme
 import cz.cvut.docta.core.presentation.preview.ScreenPreviewContainer
+import docta.composeapp.generated.resources.Res
+import docta.composeapp.generated.resources.achievement_placheholder_icon
 
 @Preview(device = Devices.PIXEL_7_PRO)
 @Composable
 fun AchievementsScreenPreview(
     appTheme: AppTheme = AppTheme.Light,
-    achievements: List<Achievement> = listOf(
-        Achievement(
+    achievements: List<AchievementUiState> = listOf(
+        AchievementUiState(
             percentage = 0.0,
             title = "The first",
             name = AchievementName.Default,
-            completed = false
+            completed = false,
+            icon = Res.drawable.achievement_placheholder_icon
         ),
-        Achievement(
-            percentage = 45.0,
+        AchievementUiState(
+            percentage = 30.0,
             title = "The first",
             name = AchievementName.Default,
-            completed = false
+            completed = false,
+            icon = Res.drawable.achievement_placheholder_icon
         ),
-        Achievement(
-            percentage = 20.0,
+        AchievementUiState(
+            percentage = 30.0,
             title = "The first",
             name = AchievementName.Default,
-            completed = false
+            completed = false,
+            icon = Res.drawable.achievement_placheholder_icon
         ),
-        Achievement(
-            percentage = 67.0,
+        AchievementUiState(
+            percentage = 50.0,
             title = "The first",
             name = AchievementName.Default,
-            completed = false
-        ),
-        Achievement(
-            percentage = 88.0,
-            title = "The first",
-            name = AchievementName.Default,
-            completed = false
+            completed = false,
+            icon = Res.drawable.achievement_placheholder_icon
         )
     )
 ) {
     ScreenPreviewContainer {
-        AchievementsScreen(achievements)
+        AchievementsScreen(achievements = achievements)
     }
 }
