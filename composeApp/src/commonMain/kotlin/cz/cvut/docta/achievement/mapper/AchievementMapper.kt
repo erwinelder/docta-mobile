19a0764e72a678106125b6c99d06ea2519d08f77
Maksym Zavada <m.zavada2005@ukr.net>
2025-03-28 12:20:26 +0100

    Update mapper & use-case implementation

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/achievement/mapper/AchievementMapper.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/achievement/mapper/AchievementMapper.kt
@@ -1,28 +1,15 @@
 package cz.cvut.docta.achievement.mapper
 
 import cz.cvut.docta.achievement.data.model.AchievementDto
-import cz.cvut.docta.achievement.data.model.AchievementNameDto
-import cz.cvut.docta.achievement.data.model.UserAchievementDto
 import cz.cvut.docta.achievement.domain.model.Achievement
 import cz.cvut.docta.achievement.domain.model.AchievementName
 
 fun achievementDataToDomainModel(
-    achievementDto: AchievementDto,
-    achievementProgress: UserAchievementDto
+    achievementDto: AchievementDto
 ): Achievement {
     return Achievement(
-        name = achievementDto.name.toAchievementNameDomainModel(),
-        title = achievementDto.title,
-        percentage = achievementProgress.percentage,
-        completed = achievementProgress.completed
+        name = AchievementName.fromKey(achievementDto.achievementName),
+        numberOfSteps = achievementDto.numberOfSteps,
+        completedSteps = achievementDto.completedSteps
     )
-}
-
-fun AchievementNameDto.toAchievementNameDomainModel(): AchievementName {
-    return when (this) {
-        AchievementNameDto.First -> AchievementName.First
-        AchievementNameDto.Second -> AchievementName.Second
-        AchievementNameDto.Third -> AchievementName.Third
-        AchievementNameDto.Default -> AchievementName.Default
-    }
 }
