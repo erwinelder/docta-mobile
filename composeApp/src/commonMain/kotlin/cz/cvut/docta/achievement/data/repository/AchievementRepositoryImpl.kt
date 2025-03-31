8d0e66d93942f134a00525a410abcdb0844582de
Maksym Zavada <m.zavada2005@ukr.net>
2025-03-31 20:23:49 +0200

    Add repository implementation

--- composeApp/src/commonMain/kotlin/cz/cvut/docta/achievement/data/repository/AchievementRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/cz/cvut/docta/achievement/data/repository/AchievementRepositoryImpl.kt
@@ -2,26 +2,36 @@ package cz.cvut.docta.achievement.data.repository
 
 import cz.cvut.docta.achievement.data.model.AchievementDto
 import cz.cvut.docta.auth.domain.model.UserContext
+import cz.cvut.docta.core.data.remote.doctaBackendUrl
+import cz.cvut.docta.core.data.remote.httpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
+import kotlinx.serialization.json.Json
 
 class AchievementRepositoryImpl(
     val userContext: UserContext
 ) : AchievementRepository {
 
     override suspend fun getAchievementsProgress(): List<AchievementDto> {
-        // TODO-ACHIEVEMENTS: Implement fetching achievements from the server
+        return try {
+            val response = httpClient.get(
+                urlString = "$doctaBackendUrl/achievement/progress"
+            ) {
+                header("Authorization", "Bearer ${userContext.getAuthToken()}")
+                contentType(ContentType.Application.Json)
+            }
 
-        return listOf(
-            AchievementDto(
-                achievementName = "educational_marathon", completedSteps = 1, numberOfSteps = 1000
-            ),
-            AchievementDto(
-                achievementName = "knowledge_is_power",
-                completedSteps = 16,
-                numberOfSteps = 32
-            ),
-            AchievementDto(
-                achievementName = "first_step", completedSteps = 1, numberOfSteps = 1
-            )
-        )
+            if (response.status == HttpStatusCode.OK) {
+                Json.decodeFromString<List<AchievementDto>>(string = response.bodyAsText())
+            } else {
+                emptyList()
+            }
+        } catch (_: Exception) {
+            emptyList()
+        }
     }
 }
